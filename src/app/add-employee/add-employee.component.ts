f45e373ff9a12d23106644013a3601cc1eb8d810
athulajayghosh <53214137+athulajayghosh@users.noreply.github.com>
2022-11-28 12:39:49 +0530

    twoway binding

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-employee.component.css']
 })
 export class AddEmployeeComponent {
+  EmpCode=""
+  name=""
+  Designation=""
+  Salary=""
+  PhoneNo=""
+  Email=""
+  CompanyWebsite=""
+  Experiance=""
 
+  readValues=()=>
+  {
+    let data:any={
+      name : this.name,
+      EmpCode:this.EmpCode,
+      Designation:this.Designation,
+      Salary:this.Salary,
+      PhoneNo:this.PhoneNo,
+      Email:this.Email,
+      CompanyWebsite:this.CompanyWebsite,
+      Experiance:this.Experiance
+      }
+      console.log(data)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
 import { DeleteEmployeeComponent } from './delete-employee/delete-employee.component';
 import { ViewAllEmployeeComponent } from './view-all-employee/view-all-employee.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 const myRoute: Routes=[
   {path:"",
   component:AdminLoginComponent
@@ -46,7 +47,8 @@ const myRoute: Routes=[
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(myRoute)
+    RouterModule.forRoot(myRoute),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
